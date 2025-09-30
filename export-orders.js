98386c0737178ad1395d2f69bd2e95f5328bbef5
Joel Chinta <116251180+joelchinta@users.noreply.github.com>
2025-09-30 14:42:40 +0800

    Update export-orders.js

--- export-orders.js
+++ export-orders.js
@@ -40,11 +40,8 @@ function formatDate(date) {
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
   const year = date.getFullYear();
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
   
-  return `${month}/${day}/${year} ${hours}:${minutes}:${seconds}`;
+  return `${month}/${day}/${year}`;
 }
 
 function buildURL(startDate, endDate) {
@@ -62,10 +59,14 @@ function buildURL(startDate, endDate) {
         "condition": "<=",
         "value": formatDate(endDate)
       }
+    },
+    {
+      "attribute": "status",
+      "value": [11]
     }
   ];
   
-  return `/v400/en/${BUSINESS_SLUG}/orders.csv?where=${encodeURIComponent(JSON.stringify(where))}`;
+  return `/v400/en/${BUSINESS_SLUG}/orders.csv?mode=dashboard&where=${encodeURIComponent(JSON.stringify(where))}&orderBy=id`;
 }
 
 function downloadCSV(url, filename) {
